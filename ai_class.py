9c8ec44df533a686a61a9b3ba4901995f563362b
lennon <hitzemanl@yahoo.com>
2024-08-10 15:49:48 -0700

    Expaned out to multiple AIs that can talk with each other. Did some cleanup on commands

--- ai_class.py
+++ ai_class.py
@@ -2,10 +2,12 @@ class ai:
     def __init__ (self,
                   name,
                   personality,
-                  background):
+                  background,
+                  ai_voice):
         self.name = name
         self.personality = personality
         self.background = background
+        self.ai_voice = ai_voice
         self.llm_messages = []
         systemMessage = {
             "role":"system",
@@ -18,7 +20,8 @@ class ai:
                 Your personality is:
                 {personality}
 
-                Everything you say has to be aligned with your personality. All responses have to be short and stay in character.
+                Everything you say has to be aligned with your personality. All responses have to be short and stay in character. No matter what you must only speak as {name} and no one else.
+                Your messages MUST begin with "[{name}] "
             """
         }
         self.llm_messages.append(systemMessage)
@@ -28,3 +31,12 @@ class ai:
 
     def getLLM_Messages(self):
         return self.llm_messages
+    
+    def getAI_Voice(self):
+        return self.ai_voice
+    
+    def getAI_Name(self):
+        return self.name
+    
+    def setLLM_Messages(self, messages):
+        self.llm_messages = messages
